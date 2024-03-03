b511f37497458986e879913f8287b0677e7a7811
Mukesh Murugan <iammukeshm@gmail.com>
2024-03-04 00:42:29 +0530

    add serverless asp.net core api on lambda hosting

--- deploying-aspnet-core-web-api-with-aws-lambda/Products.Api/Program.cs
+++ deploying-aspnet-core-web-api-with-aws-lambda/Products.Api/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 builder.Services.AddAWSService<IAmazonDynamoDB>();
 builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
